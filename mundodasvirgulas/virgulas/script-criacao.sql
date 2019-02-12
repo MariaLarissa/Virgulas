0051bdb600500ed40e50ac159fb19cbd0ebefaea
MariaLarissa <43482678+MariaLarissa@users.noreply.github.com>
2019-02-12 20:30:26 -0300

    tabela sobre as questões

--- mundodasvirgulas/virgulas/script-criacao.sql
+++ mundodasvirgulas/virgulas/script-criacao.sql
@@ -7,6 +7,12 @@ nome varchar(255),
 idade int(3),
 primary key (cod_usuario)
 );
+create table questoes (
+  cod_questao int auto_increment,
+  questao varchar(200),
+  primary key (cod_questao)
+  );
+  
 
 insert into usuarios (nome,idade)
 values ('fabio',11),
@@ -15,6 +21,22 @@ values ('fabio',11),
 ('Fê',44),
 ('Ailton',55);
 
+insert into questoes (questao)
+valeus ('“Guri que finta banco, escritório, repartição, fila, balcão, pedido de certidão, imposto a pagar.”
+
+(Lourenço Diaféria)
+
+a) Separar o aposto.
+
+b) Separar o vocativo.
+
+c) Separar orações coordenadas assindéticas.
+
+d) Separar oração subordinada adverbial da oração principal.
+
+e) Separar palavras com a mesma função sintática.');
+
 select * from usuarios;
+select * from questoes;
 
 drop database mundodasvirgulas;
